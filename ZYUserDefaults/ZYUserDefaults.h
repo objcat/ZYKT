1316a456ed765f1db0bc7e084787d080a2582317
objcat <objcat@163.com>
2023-08-04 16:15:38 +0800

    完善ZYUserDefaults使用方法

--- ZYUserDefaults/ZYUserDefaults.h
+++ ZYUserDefaults/ZYUserDefaults.h
@@ -28,3 +28,62 @@ typedef NS_ENUM(NSInteger, ZYEnvironment) {
 - (void)clean;
 
 @end
+
+
+/**
+ 使用方法
+ 
+ 新建一个类继承于`ZYUserDefaults`写入以下内容, 假如叫`ExampleUserDefaults`
+ 
+ #import "ZYUserDefaults.h"
+ #import <UIKit/UIKit.h>
+
+ #define x_store ExampleUserDefaults.shareInstance
+
+ @interface ExampleUserDefaults : ZYUserDefaults
+ @property (strong, nonatomic) NSString *testString;
+ @property (strong, nonatomic) NSNumber *testNumber;
+ @property (strong, nonatomic) NSArray *testArray;
+ @property (strong, nonatomic) NSMutableArray *testMutableArray;
+ @property (strong, nonatomic) NSDictionary *testDictionary;
+ @property (strong, nonatomic) NSMutableDictionary *testMutableDictionary;
+ @property (strong, nonatomic) NSData *testData;
+ @property (strong, nonatomic) NSMutableData *testMutableData;
+ @property (strong, nonatomic) NSDate *testDate;
+ @property (assign, nonatomic) NSTimeInterval testTimeInterval;
+ @property (assign, nonatomic) NSInteger testInteger;
+ @property (assign, nonatomic) NSUInteger testUInteger;
+ @property (assign, nonatomic) int testInt;
+ @property (assign, nonatomic) long testLong;
+ @property (assign, nonatomic) float testFloat;
+ @property (assign, nonatomic) double testDouble;
+ @property (assign, nonatomic) BOOL testBool;
+ @property (assign, nonatomic) Boolean testBoolean;
+ @property (assign, nonatomic) CGFloat testCGFloat;
+ @end
+ 
+ 然后创建pch文件, 把头文件放进去
+ 
+ 使用如下:
+ x_store.env = ZYEnvironmentDev;
+ x_store.testString = @"123";
+ x_store.testNumber = @(666);
+ x_store.testArray = @[@"1", @"2", @"3"];
+ x_store.testMutableArray = x_store.testArray.mutableCopy;
+ x_store.testDictionary = @{@"name": @"张三", @"age": @"18"};
+ x_store.testMutableDictionary = x_store.testDictionary.mutableCopy;
+ x_store.testData = [[NSData alloc] init];
+ x_store.testMutableData = [[NSMutableData alloc] init];
+ x_store.testDate = NSDate.date;
+ x_store.testTimeInterval = 1;
+ x_store.testInteger = 1;
+ x_store.testUInteger = 2;
+ x_store.testInt = 1;
+ x_store.testLong = 11;
+ x_store.testFloat = 0.2;
+ x_store.testDouble = 0.22;
+ x_store.testCGFloat = 0.222;
+ x_store.testBool = YES;
+ x_store.testBoolean = true;
+ NSLog(@"%@", x_store);
+ */
